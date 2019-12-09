9fb56bca3b74f2c5f63dfb8c33b2787e40bbfc04
zdy <zoudingyun@foxmail.com>
2019-12-09 10:15:53 +0800

    springboot框架

---
+++ src/main/java/per/zdy/intranetman/IntranetManApplication.java
@@ -0,0 +1,13 @@
+package per.zdy.intranetman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntranetManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntranetManApplication.class, args);
+    }
+
+}
---
+++ src/test/java/per/zdy/intranetman/IntranetManApplicationTests.java
@@ -0,0 +1,13 @@
+package per.zdy.intranetman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntranetManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
